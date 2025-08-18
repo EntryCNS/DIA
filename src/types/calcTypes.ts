afecbba1c06668bb5b9a834f89960470654b0023
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-08-18 20:08:23 +0900

    refactor: types

--- src/types/calcTypes.ts
+++ src/types/calcTypes.ts
@@ -20,3 +20,13 @@ export interface AddPointState {
   };
   modelAward: string;
 }
+
+//자유 학기제 여부
+export type FreeSemesterFlags = {
+  freeSem11: boolean;
+  freeSem12: boolean;
+  freeSem21: boolean;
+  freeSem22: boolean;
+  freeSem31: boolean;
+  freeSem32: boolean;
+};
